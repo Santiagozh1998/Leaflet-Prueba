12e487b54f6f166ede2022c7d4b940ff7e48a4a0
Santiago Zuluaga <santiagozh1998@gmail.com>
2019-03-13 14:48:59 -0500

    Prueba

---
+++ my-app/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Maps from './components/maps';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Maps />
+      </div>
+      
+    );
+  }
+}
+
+export default App;
